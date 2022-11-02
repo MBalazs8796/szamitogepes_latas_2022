36007216708e35934bc86378dc0bfaa9f67787af
Mosolygó Balázs <mbalazs@inf.u-szeged.hu>
2022-11-02 17:13:59 +0100

    calibration now expect 10*7 grid

--- calibrate.py
+++ calibrate.py
@@ -5,19 +5,20 @@ from tqdm import tqdm
 # termination criteria
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((10*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:10,0:7].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 images = glob.glob('chessboard/*.jpg')
 images.sort()
-
+images = images[::5]
+print(images)
 for fname in tqdm(images):
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
+    ret, corners = cv2.findChessboardCorners(gray, (10,7), None)
     # If found, add object points, image points (after refining them)
     if ret == True:
         print(fname)
