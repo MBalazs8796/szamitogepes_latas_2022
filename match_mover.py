0399e03a68e82c2d6f0fba2fb09298103ebedc80
amstadtzita <amstadtzita@gmail.com>
2022-12-02 00:25:34 +0100

    normalization of vectors to compare distance

--- match_mover.py
+++ match_mover.py
@@ -10,6 +10,7 @@ from copy import deepcopy
 from object_placer.object_loader import OBJ
 from pathlib import Path
 
+"""
 def quat2mat(q):
     ''' Calculate rotation matrix corresponding to quaternion
     Parameters
@@ -22,21 +23,27 @@ def quat2mat(q):
     '''
     _FLOAT_EPS = np.finfo(float).eps
     x, y, z, w = q
-    Nq = w*w + x*x + y*y + z*z
+    Nq = w * w + x * x + y * y + z * z
     if Nq < _FLOAT_EPS:
         return np.eye(3)
-    s = 2.0/Nq
-    X = x*s
-    Y = y*s
-    Z = z*s
-    wX = w*X; wY = w*Y; wZ = w*Z
-    xX = x*X; xY = x*Y; xZ = x*Z
-    yY = y*Y; yZ = y*Z; zZ = z*Z
+    s = 2.0 / Nq
+    X = x * s
+    Y = y * s
+    Z = z * s
+    wX = w * X;
+    wY = w * Y;
+    wZ = w * Z
+    xX = x * X;
+    xY = x * Y;
+    xZ = x * Z
+    yY = y * Y;
+    yZ = y * Z;
+    zZ = z * Z
     return np.array(
-           [[ 1.0-(yY+zZ), xY-wZ, xZ+wY ],
-            [ xY+wZ, 1.0-(xX+zZ), yZ-wX ],
-            [ xZ-wY, yZ+wX, 1.0-(xX+yY) ]])
-
+        [[1.0 - (yY + zZ), xY - wZ, xZ + wY],
+         [xY + wZ, 1.0 - (xX + zZ), yZ - wX],
+         [xZ - wY, yZ + wX, 1.0 - (xX + yY)]])
+"""
 
 def read_trajectory(filename):
     f = open(filename, 'r')
@@ -48,7 +55,7 @@ def read_trajectory(filename):
     has_pose = []
     matrix_list = []
     i = 0
-    while i < len(lines)-1:
+    while i < len(lines) - 1:
         if lines[i] in ['\n', '\r\n']:
             i += 1
             break
@@ -66,54 +73,53 @@ def read_trajectory(filename):
                 row = re.sub(r'\[|\]|\n|,|;', '', lines[i]).split()
                 matrix.append([float(v) for v in row])
                 i += 1
-            assert(len(matrix) == 4)
+            assert (len(matrix) == 4)
             matrix_list.append(matrix)
     matrix_list = np.array(matrix_list)
     has_pose = np.array(has_pose)
 
     return has_pose, matrix_list
 
+
 def close_to_any(a, floats, **kwargs):
-  return np.any(np.isclose(a, floats, **kwargs))
+    return np.any(np.isclose(a, floats, **kwargs))
+
 
 def read_kerframe_trajectory(trajectory_fn, timestamp_fn):
     with open(trajectory_fn, 'r') as fp:  # 'KeyFrameTrajectory.txt'
-      lines = fp.readlines()
+        lines = fp.readlines()
 
     with open(timestamp_fn, 'r') as fp:
-      time_lines = fp.readlines()
+        time_lines = fp.readlines()
 
     Rt_list = []
     timestamp_list = []
     for line in lines:
-      if line.strip():
-        nums = [float(n) for n in line.split(' ')]
-        timestamp = nums[0]
-        t = np.array(nums[1:4]).reshape([3,1])
-        R = Rotation.from_quat(nums[4:]).as_matrix()
-        Rt = np.hstack([R, t])
-        Rt = np.vstack([Rt, [0,0,0,1]])
-        Rt_list.append(Rt)
-        timestamp_list.append(round(float(timestamp), 2))
-
+        if line.strip():
+            nums = [float(n) for n in line.split(' ')]
+            timestamp = nums[0]
+            t = np.array(nums[1:4]).reshape([3, 1])
+            R = Rotation.from_quat(nums[4:]).as_matrix()
+            Rt = np.hstack([R, t])
+            Rt = np.vstack([Rt, [0, 0, 0, 1]])
+            Rt_list.append(Rt)
+            timestamp_list.append(round(float(timestamp), 2))
 
     has_pose = list()
     names = list()
     for line in time_lines:
-      if line.strip() and line[0] != '#':
-        tstamp, imgpth = line.split(' ')
-        names.append(imgpth.split('/')[-1].strip())
-        if round(float(tstamp), 2) in timestamp_list:
-          has_pose.append(True)
-        else:
-          has_pose.append(False)
-
+        if line.strip() and line[0] != '#':
+            tstamp, imgpth = line.split(' ')
+            names.append(imgpth.split('/')[-1].strip())
+            if round(float(tstamp), 2) in timestamp_list:
+                has_pose.append(True)
+            else:
+                has_pose.append(False)
 
     Rt_list = np.array(Rt_list)
     has_pose = np.array(has_pose)
 
     return Rt_list, has_pose, names
-
 
 
 def degree2R(roll, pitch, yaw):
@@ -122,21 +128,21 @@ def degree2R(roll, pitch, yaw):
     yaw = math.radians(yaw)
 
     yawMatrix = np.matrix([
-    [math.cos(yaw), -math.sin(yaw), 0],
-    [math.sin(yaw), math.cos(yaw), 0],
-    [0, 0, 1]
+        [math.cos(yaw), -math.sin(yaw), 0],
+        [math.sin(yaw), math.cos(yaw), 0],
+        [0, 0, 1]
     ])
 
     pitchMatrix = np.matrix([
-    [math.cos(pitch), 0, math.sin(pitch)],
-    [0, 1, 0],
-    [-math.sin(pitch), 0, math.cos(pitch)]
+        [math.cos(pitch), 0, math.sin(pitch)],
+        [0, 1, 0],
+        [-math.sin(pitch), 0, math.cos(pitch)]
     ])
 
     rollMatrix = np.matrix([
-    [1, 0, 0],
-    [0, math.cos(roll), -math.sin(roll)],
-    [0, math.sin(roll), math.cos(roll)]
+        [1, 0, 0],
+        [0, math.cos(roll), -math.sin(roll)],
+        [0, math.sin(roll), math.cos(roll)]
     ])
 
     R = yawMatrix * pitchMatrix * rollMatrix
@@ -144,29 +150,25 @@ def degree2R(roll, pitch, yaw):
     return R
 
 
-
-
 def approx_rotation(Rt):
-  """  Get legal rotation matrix """
-  # rotate teapot 90 deg around x-axis so that z-axis is up
-  Rx = np.array([[1,0,0],[0,0,-1],[0,1,0]])
+    """  Get legal rotation matrix """
+    # rotate teapot 90 deg around x-axis so that z-axis is up
+    Rx = np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]])
 
-  # set rotation to best approximation
-  R = Rt[:,:3]
-  U,S,V = np.linalg.svd(R)
-  R = np.dot(U,V)
-  R[0,:] = -R[0,:] # change sign of x-axis
+    # set rotation to best approximation
+    R = Rt[:, :3]
+    U, S, V = np.linalg.svd(R)
+    R = np.dot(U, V)
+    R[0, :] = -R[0, :]  # change sign of x-axis
 
-  # set translation
-  t = Rt[:,3].reshape(-1)
+    # set translation
+    t = Rt[:, 3].reshape(-1)
 
-  # setup 4*4 model view matrix
-  M = np.eye(4)
-  M[:3,:3] = np.dot(R,Rx)
-  M[:3,3] = t
-  return M
-
-
+    # setup 4*4 model view matrix
+    M = np.eye(4)
+    M[:3, :3] = np.dot(R, Rx)
+    M[:3, 3] = t
+    return M
 
 
 def render(img, obj, projection, h, w, color=False, scale=1):
@@ -184,45 +186,58 @@ def render(img, obj, projection, h, w, color=False, scale=1):
     img = deepcopy(img)
 
     if not hasattr(render, 'points'):
-      with open(f'./orbslam_driver/extracted/{scene_name}/result.json', 'r') as fp:
-        sm = json.load(fp)[0]
+        with open(f'./orbslam_driver/extracted/{scene_name}/result.json', 'r') as fp:
+            sm = json.load(fp)[0]
 
-      R = degree2R(sm['roll'], sm['pitch'], sm['yaw'])
-      R = np.hstack([R, [[sm['h']],[sm['w']],[sm['scale']]]])
-      R = np.vstack([R, [0,0,0,1]])
-      
-      point_list = list()
-      for face in obj.faces:
-        face_vertices = face[0]
-        points = np.array([vertices[vertex - 1] for vertex in face_vertices])
+       # R = degree2R(sm['roll'], sm['pitch'], sm['yaw'])
+       # R = np.hstack([R, [[sm['h']], [sm['w']], [sm['scale']]]])
+       # R = np.vstack([R, [0, 0, 0, 1]])
 
-        points = np.dot(points, scale_matrix)
+        point_list = list()
+        for face in obj.faces:
+            face_vertices = face[0]
+            points = np.array([vertices[vertex - 1] for vertex in face_vertices])
 
-        # render model in the middle of the reference surface. To do so,
-        # model points must be displaced
-        #points = np.array([[p[0] + w / 2, p[1] + h / 2, p[2]] for p in points])
+            points = np.dot(points, scale_matrix)
 
-        #point_list.append(cv2.perspectiveTransform(points.reshape(-1, 1, 3), R))
-        #point_list.append(points.reshape(-1, 1, 3))
-        point_list.append(points)
-      setattr(render, 'point_list', point_list)
+            # render model in the middle of the reference surface. To do so,
+            # model points must be displaced
+            # points = np.array([[p[0] + w / 2, p[1] + h / 2, p[2]] for p in points])
 
+            # point_list.append(cv2.perspectiveTransform(points.reshape(-1, 1, 3), R))
+            # point_list.append(points.reshape(-1, 1, 3))
+            point_list.append(points)
+        setattr(render, 'point_list', point_list)
+
+    t = projection[:, 2]
+    projection_norm_l2 = np.linalg.norm(t)
+    global a12a13_vector, t_0
+    #print("t", t)
+    #print("t_0", t_0)
+
+    if projection_norm_l2 > a12a13_vector or projection_norm_l2 > norm_l2_avg:
+        projection[0,3] = t_0[0]
+        projection[1,3] = t_0[1]
+
+    #print("proj", projection_norm_l2)
+    #print("a12a13_vector", a12a13_vector)
+    #print("norm_l2_avg", norm_l2_avg)
 
     for points in render.point_list:
 
-        #print(points)
-        #print(projection)
-        #print(points.reshape(-1, 1, 3).shape)
-        #print("################################")
+        # print(points)
+        # print(projection)
+        # print(points.reshape(-1, 1, 3).shape)
+        # print("################################")
         dst = cv2.transform(points.reshape(-1, 1, 3), projection)
-        #print(dst.shape)
-        #dst = dst @ np.diag([0.1, 0.1, 1])
-        
-        #for point in points:
-          
-        #print(points.reshape(3,3))
-        #print(dst)
-        #exit(69)
+        # print(dst.shape)
+        # dst = dst @ np.diag([0.1, 0.1, 1])
+
+        # for point in points:
+
+        # print(points.reshape(3,3))
+        # print(dst)
+        # exit(69)
         imgpts = np.int32(dst)
         r = list()
         for f in imgpts:
@@ -236,15 +251,17 @@ def render(img, obj, projection, h, w, color=False, scale=1):
             color = cv2.hex_to_rgb(face[-1])
             color = color[::-1]  # reverse
             cv2.fillConvexPoly(img, imgpts, color)
-    #print('Render Success')
+    # print('Render Success')
+    a12a13_vector = projection_norm_l2
+    t_0 = t
 
     return img
 
 
 def _bg_filename_sort_key(filename: str):
-  basename = Path(filename).name
-  basename_parts_without_extension = basename.split('.')[:-1]
-  return float('.'.join(basename_parts_without_extension))
+    basename = Path(filename).name
+    basename_parts_without_extension = basename.split('.')[:-1]
+    return float('.'.join(basename_parts_without_extension))
 
 
 if __name__ == '__main__':
@@ -257,36 +274,34 @@ if __name__ == '__main__':
     camera_config_name = args.config
 
     with open(f'./orbslam_driver/extracted/{scene_name}/result.json', 'r') as fp:
-      sm = json.load(fp)[0]
-
+        sm = json.load(fp)[0]
 
     # read file
     bg_filenames = glob.glob(f'./orbslam_driver/extracted/{scene_name}/rgb/*.png')
     bg_filenames.sort(key=_bg_filename_sort_key)
-    poses, has_pose, names = read_kerframe_trajectory(f'./orbslam_driver/extracted/{scene_name}/KeyFrameTrajectory.txt', f'./orbslam_driver/extracted/{scene_name}/rgb.txt')
+    poses, has_pose, names = read_kerframe_trajectory(f'./orbslam_driver/extracted/{scene_name}/KeyFrameTrajectory.txt',
+                                                      f'./orbslam_driver/extracted/{scene_name}/rgb.txt')
 
     video_path = f'./vids/{scene_name}.mp4'
-           
-    
+
     video = cv2.VideoCapture(video_path)
-    
+
     ret = True
-    
+
     if not video.isOpened():
         print('Error opening video')
         exit(69)
 
-              
     bg_filenames = np.array(bg_filenames)
     bg_filenames = bg_filenames[has_pose]
-    
+
     # frame_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
     # frame_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
     # fps = int(video.get(cv2.CAP_PROP_FPS))
-    
+
     # print(frame_width)
     # print(frame_height)
-    
+
     # writer = cv2.VideoWriter(f'{scene_name}_match_move.avi', cv2.VideoWriter_fourcc(*'XVID'), 49, (frame_width, frame_height))
     # while video.isOpened():
     #   s,f = video.read()
@@ -295,8 +310,8 @@ if __name__ == '__main__':
     #     break
     # writer.release()
     # exit(69)
-    
-    assert(len(poses) == len(bg_filenames))
+
+    assert (len(poses) == len(bg_filenames))
 
     n_frames = len(poses)
 
@@ -305,60 +320,87 @@ if __name__ == '__main__':
 
     # Camera Intrinsics
     if camera_config_name == 'config_mb':
-      im_w, im_h = 1280, 720
-      fps = 5
-      K =  np.array([ [870.25319293,  0, 637.28771858],
+        im_w, im_h = 1280, 720
+        fps = 5
+        K = np.array([[870.25319293, 0, 637.28771858],
                       [0, 866.48681104, 354.55971258],
-                      [0,       0,   1]])
+                      [0, 0, 1]])
     else:
-      raise ValueError(f'Camera config with name {camera_config_name} not exists')
+        raise ValueError(f'Camera config with name {camera_config_name} not exists')
 
     # Convert K [3,3] to [4,4]
-    K = np.hstack([K, np.zeros([3,1])])
-    K = np.vstack([K, [0,0,0,1]])
+    K = np.hstack([K, np.zeros([3, 1])])
+    K = np.vstack([K, [0, 0, 0, 1]])
 
+    #maximum mozgas
+    norm_l2 = 0
+    #   P
+    #   a00 + a10 + a20 + a[12]
+    #   a01 + a11 + a21 + a[13]
+    #   a02 + a12 + a22 + a[14]
+    #   a03 + a13 + a23+ a[15]
+    for i in range(5):
+        R_model = degree2R(roll=0, pitch=0, yaw=0)
+        t_model = np.array([[0, 0, -1]]).T  # 14000
+        Rt_model = np.hstack([R_model, t_model])
+        Rt_model = np.vstack([Rt_model, [0, 0, 0, 1]])
+
+        # Camera Pose
+        Rt_cam = poses[i]
+        Rt_cam = np.linalg.pinv(Rt_cam)
+        Rt = Rt_cam @ Rt_model
+        P = K @ Rt
+
+        T = P[ :,2]
+        norm_l2 += np.linalg.norm(T)
+    #elso 5 mozgast leiro vektor 2. norma atlaga
+    norm_l2_avg = norm_l2 / 5
+    a12a13_vector = norm_l2 / 5
+    t_0 = T
+    #print(norm_l2_avg)
+
+    #################################################
     result_imgs = []
     for i in range(n_frames):
-      print('='*10)
-      print('Frame: ', i)
+        print('=' * 10)
+        print('Frame: ', i)
 
-      img = cv2.imread(bg_filenames[i])
+        img = cv2.imread(bg_filenames[i])
 
+        # t_model = [[sm['h']],[sm['w']],[-500]]
+        # R_model = degree2R(sm['roll'], sm['pitch'], sm['yaw'])
+        R_model = degree2R(roll=0, pitch=0, yaw=0)
+        t_model = np.array([[0, 0, -1]]).T  # 14000
+        Rt_model = np.hstack([R_model, t_model])
+        Rt_model = np.vstack([Rt_model, [0, 0, 0, 1]])
 
-      #t_model = [[sm['h']],[sm['w']],[-500]]
-      #R_model = degree2R(sm['roll'], sm['pitch'], sm['yaw'])
-      R_model = degree2R(roll=0, pitch=0, yaw=0)
-      t_model = np.array([[0, 0, -2]]).T  # 14000
-      Rt_model = np.hstack([R_model, t_model]) 
-      Rt_model = np.vstack([Rt_model, [0,0,0,1]])
+        # Rt_model = Rt_model @ R
 
-      #Rt_model = Rt_model @ R
+        # Camera Pose
+        Rt_cam = poses[i]
+        # print(Rt_cam)
+        # Rt_cam = cv2.invertAffineTransform(Rt_cam)
+        Rt_cam = np.linalg.pinv(Rt_cam)
 
-      # Camera Pose
-      Rt_cam = poses[i]
-      #print(Rt_cam)
-      #Rt_cam = cv2.invertAffineTransform(Rt_cam)
-      Rt_cam = np.linalg.pinv(Rt_cam)
-      
-      #Rt_cam = np.linalg.pinv(Rt_cam)
-      Rt = Rt_cam @ Rt_model
-      #Rt = approx_rotation(Rt[:-1, :])
-      #Rt = np.linalg.pinv(Rt)
-      #print('Rt', Rt, end='\n\n')
-      P = K @ Rt
+        # Rt_cam = np.linalg.pinv(Rt_cam)
+        Rt = Rt_cam @ Rt_model
+        # Rt = approx_rotation(Rt[:-1, :])
+        # Rt = np.linalg.pinv(Rt)
+        # print('Rt', Rt, end='\n\n')
+        P = K @ Rt
 
-      #P = Rt
-      #print('P', P, end='\n\n')
-      P = P[:-1, :]  # [4,4] -> [3,4]
-      img = render(img, obj, P, h=1, w=1, color=False, scale=1)
-      # img = render(img, obj, Rt_model, h=0, w=0, color=False)
+        # P = Rt
+        # print('P', P, end='\n\n')
+        P = P[:-1, :]  # [4,4] -> [3,4]
+        img = render(img, obj, P, h=1, w=1, color=False, scale=1)
+        # img = render(img, obj, Rt_model, h=0, w=0, color=False)
 
-      cv2.imshow(scene_name, img)
-      if cv2.waitKey() == ord('q'):
-       break
-      result_imgs.append(img)
+        cv2.imshow(scene_name, img)
+        if cv2.waitKey() == ord('q'):
+            break
+        result_imgs.append(img)
     writer = cv2.VideoWriter(f'{scene_name}_match_move.avi', cv2.VideoWriter_fourcc(*'XVID'), fps, (im_w, im_h))
     for i in result_imgs:
-      writer.write(i)
+        writer.write(i)
     writer.release()
     cv2.destroyAllWindows()
